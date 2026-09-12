91b3b4207858cdd56ed16dad751ef92f4afd72b5
tempestas <62885176+NoDayLight@users.noreply.github.com>
2026-09-12 15:23:02 +0300

    v1.4: expose serialized LCD reload helper

--- lcd/hooks.h
+++ lcd/hooks.h
@@ -4,6 +4,8 @@
 
 int lcd_enable_hooks(void);
 void lcd_disable_hooks(void);
+/* Caller must hold the global state lock. */
+int lcd_reload_backend(void);
 
 int vitabrightLcdGetBrightnessValues(uint8_t out[LCD_LUT_LEVELS]);
 int vitabrightLcdSetBrightnessValues(uint8_t in[LCD_LUT_LEVELS]);
