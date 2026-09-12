8a269144073bbfa299a6f02d87827bd936fa075c
tempestas <62885176+NoDayLight@users.noreply.github.com>
2026-09-12 18:09:22 +0300

    parser: add OLED streaming parser API

--- lut_parser_core.h
+++ lut_parser_core.h
@@ -15,3 +15,17 @@ void vbe_lcd_lut_parser_init(VbeLcdLutParser *parser, uint8_t out[LCD_LUT_LEVELS
 int vbe_lcd_lut_parser_feed(VbeLcdLutParser *parser, unsigned char c);
 int vbe_lcd_lut_parser_finish(VbeLcdLutParser *parser);
 int vbe_lcd_lut_values_valid(const uint8_t values[LCD_LUT_LEVELS]);
+
+typedef struct {
+    uint8_t *out;
+    uint8_t row[LUT_LINE_SIZE];
+    unsigned int high_nibble;
+    int rows;
+    int cols;
+    int state;
+    int failed;
+} VbeOledLutParser;
+
+void vbe_oled_lut_parser_init(VbeOledLutParser *parser, uint8_t out[LUT_SIZE]);
+int vbe_oled_lut_parser_feed(VbeOledLutParser *parser, unsigned char c);
+int vbe_oled_lut_parser_finish(VbeOledLutParser *parser);
