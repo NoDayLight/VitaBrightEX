a7d1169fe5940900410deba1863e9aa8df0ecdd7
tempestas <62885176+NoDayLight@users.noreply.github.com>
2026-09-12 16:10:54 +0300

    api: expose atomic LCD LUT persistence

--- lcd/hooks.h
+++ lcd/hooks.h
@@ -9,4 +9,5 @@ int lcd_reload_backend(void);
 
 int vitabrightLcdGetBrightnessValues(uint8_t out[LCD_LUT_LEVELS]);
 int vitabrightLcdSetBrightnessValues(uint8_t in[LCD_LUT_LEVELS]);
+int vitabrightLcdPersistBrightnessValues(void);
 int vitabrightLcdReapplyColor(void);
