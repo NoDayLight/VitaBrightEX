e768c4329a43336fa2a96982a1f5a40a2751525a
tempestas <62885176+NoDayLight@users.noreply.github.com>
2026-09-12 18:14:02 +0300

    test: add LUT parser host test skeleton

---
+++ tests/lut_parser_host.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../lut_parser_core.h"
+
+int main(void) {
+    puts("production LUT parser regressions: placeholder");
+    return 0;
+}
