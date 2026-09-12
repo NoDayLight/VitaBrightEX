4cb2d5859178f7dae53f3268c838fa8b758233d3
tempestas <62885176+NoDayLight@users.noreply.github.com>
2026-09-12 15:09:24 +0300

    v1.4: return LCD backend initialization result

--- lcd/hooks.h
+++ lcd/hooks.h
@@ -2,10 +2,9 @@
 #include <stdint.h>
 #include "lcd_lut.h"
 
-void lcd_enable_hooks(void);
+int lcd_enable_hooks(void);
 void lcd_disable_hooks(void);
 
-/* Syscall exports */
 int vitabrightLcdGetBrightnessValues(uint8_t out[LCD_LUT_LEVELS]);
 int vitabrightLcdSetBrightnessValues(uint8_t in[LCD_LUT_LEVELS]);
 int vitabrightLcdReapplyColor(void);
