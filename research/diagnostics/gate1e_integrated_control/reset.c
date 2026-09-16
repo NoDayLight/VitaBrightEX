b7236cd5b2554efee178c1f666ebda5921f1da00
tempestas <62885176+NoDayLight@users.noreply.github.com>
2026-09-16 09:37:15 +0300

    test: add integrated immediate reset client

---
+++ research/diagnostics/gate1e_integrated_control/reset.c
@@ -0,0 +1,12 @@
+#include "common.h"
+
+int main(int argc, char **argv) {
+    VbeGate1eActionBundle b = {0};
+    int ret;
+    (void)argc; (void)argv;
+    b.action_result = vitabrightMatrixReset();
+    ret = vitabrightMatrixGetStatus(&b.status);
+    if (ret < 0) return 10;
+    ret = gate1e_write_file("ux0:data/vbe_gate1e_integrated_reset.bin", &b, sizeof(b));
+    return ret < 0 ? 11 : 0;
+}
